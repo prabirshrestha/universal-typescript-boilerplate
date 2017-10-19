f0fc076d2265159f7d7a817e476bec055cbe748a
Prabir Shrestha <mail@prabir.me>
2017-10-18 23:02:04 -0700

    add koa-compress

--- src/server/index.ts
+++ src/server/index.ts
@@ -8,6 +8,7 @@ import * as path from 'path';
 
 import * as Koa from 'koa';
 import * as serve from 'koa-static';
+import * as compress from 'koa-compress';
 import * as Router from 'koa-router';
 
 const app = new Koa();
@@ -22,6 +23,15 @@ app.use(async (ctx, next) => {
   ctx.set('X-Response-Time', `${ms}ms`);
 });
 
+app.use(compress({
+  filter: function (content_type) {
+    return /text/i.test(content_type)
+      || /application\/javascript/i.test(content_type)
+      || /application\/json/i.test(content_type);
+  },
+  threshold: 1,
+  flush: require('zlib').Z_SYNC_FLUSH
+}));
 app.use(serve(path.join(__dirname, '../public')));
 app.use(router.routes());
 
